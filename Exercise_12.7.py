c0a72a710f8abff0d2841995d8eb0c8b6e1722e7
SukortsevaNatalia <118372996+SukortsevaNatalia@users.noreply.github.com>
2022-12-16 22:22:37 +0200

    Add files via upload

---
+++ Exercise_12.7.py
@@ -0,0 +1,9 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Сумма, которую вы планируете положить под проценты:"))
+
+deposit = []
+for value in per_cent:
+    deposit.append(int(per_cent[value]*money/100))
+print(deposit)
+max_deposit = max(deposit)
+print('Максимальная сумма, которую вы можете заработать - '+ str(max_deposit))
